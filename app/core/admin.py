06218908fe94df967d8ef5bab152520819cc7c5e
azabarnahaju <kanikalexandra@gmail.com>
2024-06-03 15:16:39 +0200

    Add change user profile page customization in admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,6 +3,7 @@ Dajngo admin customization.
 """
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from core import models
 
@@ -11,5 +12,38 @@ class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
     ordering = ['id']
     list_display = ['email', 'first_name', 'last_name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Basic data'),
+            {
+                'fields': (
+                    'first_name',
+                    'last_name',
+                    'role'
+                )
+            }
+        ),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser'
+                )
+            }
+        ),
+        (
+            _('Important dates'),
+            {
+                'fields': (
+                    'last_login',
+                    'date_joined'
+                )
+            }
+        )
+    )
+    readonly_fields = ['last_login', 'date_joined', 'role']
 
 admin.site.register(models.User, UserAdmin)
