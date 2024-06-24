c2a9b288822bab596a2951ff2115b0510d9edb92
azabarnahaju <kanikalexandra@gmail.com>
2024-06-24 13:11:36 +0200

    Add tech and personal skill enum endpoints

--- app/core/urls.py
+++ app/core/urls.py
@@ -5,10 +5,20 @@ from django.urls import path
 
 from .views import (
     LangSkillListView,
-    LangProfListView
+    LangProfListView,
+    TechSkillListView,
+    PersSkillListView
 )
 
 urlpatterns = [
     path('languages', LangSkillListView.as_view(), name='language-list'),
-    path('language-levels', LangProfListView.as_view(), name='language-levels')
+    path('language-levels',
+         LangProfListView.as_view(),
+         name='language-levels'),
+    path('technical-skills',
+         TechSkillListView.as_view(),
+         name='technical-skills'),
+    path('personal-skills',
+         PersSkillListView.as_view(),
+         name='personal-skills')
 ]
