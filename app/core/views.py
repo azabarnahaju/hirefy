047d0b37c3199578ab2df1474ffeb7480e20da91
azabarnahaju <kanikalexandra@gmail.com>
2024-06-24 13:10:26 +0200

    Add tech and personal skill views and fix language views

--- app/core/views.py
+++ app/core/views.py
@@ -1,31 +1,66 @@
 """
 Views for the enum objects.
 """
-from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin
 from rest_framework.response import Response
 from .enums import (
     LangProf,
-    LangSkill
+    LangSkill,
+    TechSkill,
+    PersSkill
 )
 from .serializers import (
     LanguageLevelSerializer,
-    LanguageSerializer
+    LanguageSerializer,
+    TechnicalSkillEnumSerializer,
+    PersonalSkillEnumSerializer
 )
 
 
-class LangSkillListView(APIView):
+class LangSkillListView(GenericAPIView, ListModelMixin):
     """View for listing all the languages."""
-    def get(self, request):
+    serializer_class = LanguageSerializer
+
+    def get(self, request, *args, **kwargs):
         choices = LangSkill.choices
         data = [{'value': choice[0], 'label': choice[1]} for choice in choices]
-        serializer = LanguageSerializer(data, many=True)
+        serializer = self.get_serializer(data=data, many=True)
+        serializer.is_valid()
         return Response(serializer.data)
 
 
-class LangProfListView(APIView):
+class LangProfListView(GenericAPIView, ListModelMixin):
     """View for listing all the language levels."""
-    def get(self, request):
+    serializer_class = LanguageLevelSerializer
+
+    def get(self, request, *args, **kwargs):
         choices = LangProf.choices
         data = [{'value': choice[0], 'label': choice[1]} for choice in choices]
-        serializer = LanguageLevelSerializer(data, many=True)
+        serializer = self.get_serializer(data=data, many=True)
+        serializer.is_valid()
+        return Response(serializer.data)
+
+
+class TechSkillListView(GenericAPIView, ListModelMixin):
+    """View for listing all the technical skills."""
+    serializer_class = TechnicalSkillEnumSerializer
+
+    def get(self, request, *args, **kwargs):
+        choices = TechSkill.choices
+        data = [{'value': choice[0], 'label': choice[1]} for choice in choices]
+        serializer = self.get_serializer(data=data, many=True)
+        serializer.is_valid()
+        return Response(serializer.data)
+
+
+class PersSkillListView(GenericAPIView, ListModelMixin):
+    """View for listing all the personal skills."""
+    serializer_class = PersonalSkillEnumSerializer
+
+    def get(self, request, *args, **kwargs):
+        choices = PersSkill.choices
+        data = [{'value': choice[0], 'label': choice[1]} for choice in choices]
+        serializer = self.get_serializer(data=data, many=True)
+        serializer.is_valid()
         return Response(serializer.data)
