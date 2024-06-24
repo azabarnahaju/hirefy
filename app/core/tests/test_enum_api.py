5f2b8f1baed1268bf5054ab9fe8764cf85bc3ec1
azabarnahaju <kanikalexandra@gmail.com>
2024-06-24 13:08:49 +0200

    Add enum API tests for technical and personal skills

--- app/core/tests/test_enum_api.py
+++ app/core/tests/test_enum_api.py
@@ -9,6 +9,8 @@ from rest_framework.test import APIClient
 
 LANGUAGES_URL = reverse('language-list')
 LANGUAGE_LEVEL_URL = reverse('language-levels')
+TECHNICAL_SKILL_URL = reverse('technical-skills')
+PERSONAL_SKILL_URL = reverse('personal-skills')
 
 
 class EnumTests(TestCase):
@@ -26,5 +28,17 @@ class EnumTests(TestCase):
     def test_get_method_returns_language_levels(self):
         """Test that a GET method returns all the language levels."""
         res = self.client.get(LANGUAGE_LEVEL_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_get_method_returns_technical_skills(self):
+        """Test that a GET method returns all the technical skills."""
+        res = self.client.get(TECHNICAL_SKILL_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_get_method_returns_personal_skills(self):
+        """Test that a GET method returns all the personal skills."""
+        res = self.client.get(PERSONAL_SKILL_URL)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
