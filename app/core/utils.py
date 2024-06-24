dd3b245535e2196a01e87b4baeb31735ae6dabd8
azabarnahaju <kanikalexandra@gmail.com>
2024-06-24 13:11:00 +0200

    Add technical and personal skill get_or_create utils

--- app/core/utils.py
+++ app/core/utils.py
@@ -2,7 +2,11 @@
 Utility functions.
 """
 
-from .models import LanguageSkill
+from .models import (
+    LanguageSkill,
+    TechnicalSkill,
+    PersonalSkill
+)
 
 
 def get_or_create_language_skills(language_data):
@@ -11,3 +15,17 @@ def get_or_create_language_skills(language_data):
         level=language_data['level']
     )
     return language_skill
+
+
+def get_or_create_technical_skills(tech_skill_data):
+    tech_skill, created = TechnicalSkill.objects.get_or_create(
+        value=tech_skill_data['value']
+    )
+    return tech_skill
+
+
+def get_or_create_personal_skills(pers_skill_data):
+    pers_skill, created = PersonalSkill.objects.get_or_create(
+        value=pers_skill_data['value']
+    )
+    return pers_skill
