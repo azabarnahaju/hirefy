72387b0c3cae11ff5473fb31b1f49941204440f1
azabarnahaju <kanikalexandra@gmail.com>
2024-06-24 13:09:29 +0200

    Add technical and personal skill serializers

--- app/core/serializers.py
+++ app/core/serializers.py
@@ -3,7 +3,11 @@ Serializers for the enum objects.
 """
 from rest_framework import serializers
 
-from .models import LanguageSkill
+from .models import (
+    LanguageSkill,
+    TechnicalSkill,
+    PersonalSkill
+)
 
 
 class LanguageSerializer(serializers.Serializer):
@@ -18,9 +22,37 @@ class LanguageLevelSerializer(serializers.Serializer):
     label = serializers.CharField()
 
 
+class TechnicalSkillEnumSerializer(serializers.Serializer):
+    """Serializer for the technical skills enum."""
+    value = serializers.CharField()
+    label = serializers.CharField()
+
+
+class PersonalSkillEnumSerializer(serializers.Serializer):
+    """Serializer for the personal skills enum."""
+    value = serializers.CharField()
+    label = serializers.CharField()
+
+
 class LanguageSkillSerializer(serializers.ModelSerializer):
     """Serializer for the the LanguageSkill model."""
 
     class Meta:
         model = LanguageSkill
         fields = ['language', 'level']
+
+
+class TechnicalSkillSerializer(serializers.ModelSerializer):
+    """Serializer for the technical skill."""
+
+    class Meta:
+        model = TechnicalSkill
+        fields = '__all__'
+
+
+class PersonalSkillSerializer(serializers.ModelSerializer):
+    """Serializer for the personal skill."""
+
+    class Meta:
+        model = PersonalSkill
+        fields = '__all__'
